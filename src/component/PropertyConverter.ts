904044cdec0d1decf2b422c083c649046634d969
XiaoMing(Justin) <117292775@qq.com>
2015-01-20 16:12:28 +0800

    updated

--- src/component/PropertyConverter.ts
+++ src/component/PropertyConverter.ts
@@ -7,6 +7,10 @@ module WOZLLA.component {
             return new WOZLLA.math.Rectangle(arr[0], arr[1], arr[2], arr[3]);
         }
 
+        public static array2circle(arr:Array<number>):WOZLLA.math.Circle {
+            return new WOZLLA.math.Circle(arr[0], arr[1], arr[2]);
+        }
+
     }
 
 }
